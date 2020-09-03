6d14e3ed77cc862a908095eba4e2c2ef063a7818
Akram Chauhan <akram.aic193@gmail.com>
2020-09-03 23:09:32 +0530

    Updating User Seeder in order to update Bio and Location data

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -20,8 +20,8 @@ class UsersSeeder extends Seeder
                 'phone_number'=>'999-888-7654',
                 'profile_picture'=>'headshot_full.png',
                 'slug'=>'antonio',
-                'address'=>'123 Main Street',
-                'bio'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+                'address'=>'Naples, Italy',
+                'bio'=>'This is a bio, empty but always a bio.',
                 'password'=>bcrypt('password'),
                 'created_at'=>'2019-08-07 06:26:02',
                 'updated_at'=>'2019-09-20 05:04:13'
